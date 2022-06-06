7cf085fef03b660362c7e967bec1e17250033390
Nicolas Mariano <nicmariano10@gmail.com>
2022-06-05 22:10:12 -0300

    add class jump and remove in 5 seconds

---
+++ js/scripty.js
@@ -0,0 +1,14 @@
+let mario = document.querySelector(".mario")
+ const jump = ()=>{
+     mario.classList.add("jump")
+ 
+ setTimeout(() => {
+ 
+    mario.classList.remove("jump")
+ 
+    },500)
+    }
+
+
+
+document.addEventListener("keydown",jump)

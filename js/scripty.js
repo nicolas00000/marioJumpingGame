1eeb6e09b58a45033f559d3be79869b8a27b7900
Nicolas Mariano <nicmariano10@gmail.com>
2022-06-06 17:54:16 -0300

    validação de pulo e game over trocando imagem do personagem

--- js/scripty.js
+++ js/scripty.js
@@ -1,4 +1,6 @@
 let mario = document.querySelector(".mario")
+let pipe = document.querySelector(".pipe")
+
  const jump = ()=>{
      mario.classList.add("jump")
  
@@ -9,6 +11,24 @@ let mario = document.querySelector(".mario")
     },500)
     }
 
+    document.addEventListener("keydown",jump)
 
 
-document.addEventListener("keydown",jump)
+    const loop = setInterval(() => {                            //transformar string em number
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '') 
+        const pipePosition = pipe.offsetLeft
+   
+        if(pipePosition <= 140 && pipePosition > 0 && marioPosition < 100){
+            pipe.style.animation = "none"
+            pipe.style.left = `${pipePosition}px`
+        
+            mario.style.animation = "none"
+            mario.style.bottom = `${marioPosition}px`
+        
+            mario.src = "img/game-over.png"
+            mario.style.width = "100px"
+            mario.style.marginLeft= "50px"
+        
+        }
+   
+    }, 10);
